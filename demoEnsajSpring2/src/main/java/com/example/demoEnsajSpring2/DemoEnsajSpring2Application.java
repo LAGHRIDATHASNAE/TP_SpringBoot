dd5400087281673623ac05bc9972dee1050de039
LAGHRIDATHASNAE <148015530+LAGHRIDATHASNAE@users.noreply.github.com>
2023-10-23 21:50:02 +0100

    Add files via upload

---
+++ demoEnsajSpring2/src/main/java/com/example/demoEnsajSpring2/DemoEnsajSpring2Application.java
@@ -0,0 +1,17 @@
+package com.example.demoEnsajSpring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoEnsajSpring2Application {
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEnsajSpring2Application.class, args);
+	}
+
+}
